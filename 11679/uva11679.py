e236248fc2d06172f598b4e59d65e1ca377f2dc3
Marcelo Amancio <marceloamancio@gmail.com>
2018-02-18 23:16:29 -0300

    uva 11679 solved

---
+++ 11679/uva11679.py
@@ -0,0 +1,25 @@
+# UVA10035 - Sub-prime - Accepted
+# goo.gl/788UiP
+# @author: Marcelo Adriano Amancio
+# 'Rather go to bed with out dinner than to rise in debt.' -- Benjamin Franklin
+from sys import stdin
+
+if __name__ == '__main__':
+    i = 0
+    while True:
+        i += 1
+        b,n = [int(x) for x in stdin.readline().split()]
+        if b+n == 0: break
+
+        banks_cash = [int(x) for x in stdin.readline().split()]
+        for _ in range(n):
+            d,c,v = [int(x) for x in stdin.readline().split()]
+            d,c = d-1, c-1
+            banks_cash[d] -= v
+            banks_cash[c] += v
+        possible = True
+        for e in banks_cash:
+            if e<0:
+                possible = False
+                break
+        print('S' if possible else 'N')
